01aeef9db23c2288af7b6d3a135cc19387306785
lache-sys <program6493@outlook.com>
2026-06-01 19:42:12 +0900

    modified:   .config/home-manager/home/.woodpecker/check.yml new file:   cfg/tlmgr.txt new file:   cfg/uv.txt modified:   install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 set -eou pipefail
 HM_SRC="$(cd "$(dirname "$0")"; pwd)/.config/home-manager"
 HM_TGT="${HOME}/.config/home-manager"
+SCR_DIR="$(cd "$(dirname "$0")"; pwd)"
 if [[ "$(uname)" == "Darwin" ]]; then
   PKGTMPDIR=$(mktemp -d)
   PKGTMP=${PKGTMPDIR}/Determinate.pkg
   curl -L -o ${PKGTMP} https://install.determinate.systems/determinate-pkg/stable/Universal
   sudo installer -pkg ${PKGTMP} -target /
   sudo rm -rf ${PKGTMPDIR}
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   if [[ $(uname -m) == "arm64" || $(uname -m) == "aarch64" ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
   elif [[ $(uname -m) == "x86_64" ]]; then
@@ -33,6 +34,16 @@ nix run home-manager/master -- switch --flake .
 home-manager switch --flake .
 if [[ "$(uname)" == "Darwin" ]]; then
   sudo nix run nix-darwin -- switch --flake .#lache-sys-darwin
-  UV_PYTHON=$(brew --prefix)/bin/python3 uv venv
+  UV_VENV_CLEAR=1 UV_PYTHON=$(brew --prefix)/bin/python3 uv venv
+elif [[ "$(uname)" == "Linux" ]]; then
+  UV_VENV_CLEAR=1 uv venv
 fi
+if $(which tlmgr); then
+  sudo tlmgr update --self
+  while IFS= read line; do
+    sudo tlmgr install ${line}
+  done < "${SCR_DIR}/cfg/tlmgr.txt"
+fi
+source ~/.venv/bin/activate
+uv pip install -r "${SCR_DIR}/cfg/uv.txt"
 exit 0
