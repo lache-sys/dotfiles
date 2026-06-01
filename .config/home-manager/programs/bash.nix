8e6fbf1cbf2e11cd3798d2d8459b4e7e6a679d3a
lache-sys <program6493@outlook.com>
2026-06-01 10:20:36 +0900

    modified:   .config/home-manager/programs/bash.nix modified:   .config/home-manager/programs/zsh.nix

--- .config/home-manager/programs/bash.nix
+++ .config/home-manager/programs/bash.nix
@@ -5,6 +5,11 @@
     bash = {
       enable = true;
       enableCompletion = true;
+      initContent = ''
+        if [[ "$(uname)" == "Darwin" ]]; then
+          SSH_AUTH_SOCK = "${config.home.homeDirectory}/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock";
+        fi
+      '';
     };
   };
 }
